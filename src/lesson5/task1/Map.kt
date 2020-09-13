2dda83f0eab5b527c08f8a341410dd64c49b120f
NkyCHRN <14nik20@gmail.com>
2020-09-13 17:50:34 +0300

    Решены все задачи 1-го, 2-го, 3-го и 4-го уроков + добавлены новые тесты. Также решены все задачи простой, средней и очень сложной сложности 5-го урока, некоторые сложные.

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -390,11 +390,9 @@ fun bagPacking(treasures: Map<String, Pair<Int, Int>>, capacity: Int): Set<Strin
             capacityNow -= weight[i - 1]
         }
     }
-    println(itemsList)
     val selected: MutableSet<String> = mutableSetOf()
     var nextPair = false
     for (s in itemsList.indices) {
-        println(s)
         for ((k, v) in treasures) {
             if (v == itemsList[s] && k !in selected) {
                 selected.add(k)
